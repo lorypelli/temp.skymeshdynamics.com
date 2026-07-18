0ef3257bf903537036b8dfb96266bf0a37b10533
lorypelli <lorypelli@proton.me>
2026-07-18 08:23:50 +0200

    run vite on port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import devServer from '@hono/vite-dev-server';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        port: 3000,
+    },
     plugins: [
         devServer({ entry: 'src/index.ts' }),
         build({ entry: 'src/index.ts', emptyOutDir: true }),
